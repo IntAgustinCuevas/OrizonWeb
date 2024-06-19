5576588f0abfcd869f4fe6e8e588b0633e883042
Agustin <Agustin2010_c@hotmail.com>
2024-06-19 17:25:03 -0300

    Untested user registration

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,6 @@
 import User from '../models/userModel.js';
 import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 
---
+++ src/routes/userRouter.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import userController from '../controllers/userController.js';
+import validationMiddleware from '../middlewares/validationMiddleware.js';
+
+const userRouter = express.Router();
+
+// Endpoint for register new user //
+userRouter.post('/register', validationMiddleware.validationSignUp, userController.createUser);
+
+export default userRouter;
