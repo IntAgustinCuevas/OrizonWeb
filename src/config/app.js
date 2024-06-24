03d4d4e949b79b2f8642196a9cd32da6910190b1
Agustin <Agustin2010_c@hotmail.com>
2024-06-24 17:13:36 -0300

    Added routes to serve static files

--- src/config/app.js
+++ src/config/app.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import userRouter from '../routes/userRouter.js';
+import viewRouter from '../routes/viewRouter.js';
 
+// Configuracion //
 const app = express();
 
-app.use(express.json);
+// Middlewares globales //
+app.use(express.json());
 
+// Montado de endpoints de la API //
 app.use('/api/users' , userRouter);
+app.use('/view' , viewRouter)
 
 export default app;
